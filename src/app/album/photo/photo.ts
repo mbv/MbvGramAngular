1f214ebc5806de34bda8256f13c6b3bc57a2fb99
Konstantin Terehov <KonstantinTerehov@coherentsolutions.com>
2017-05-12 17:13:31 +0300

    Fix bugs with photos

--- src/app/album/photo/photo.ts
+++ src/app/album/photo/photo.ts
@@ -1,11 +1,10 @@
 export class Photo {
-  constructor(
-    public id?: number,
-    public album_id?: number,
-    public photo?: { url?:string},
-    public description?: string,
-    public tag_list?: string[],
-    public created_at?: string,
-    public updated_at?: string,
-  ) {}
+  constructor(public id?: number,
+              public album_id?: number,
+              public photo?: { url?: string, thumb: { url?: string }, small_thumb: { url?: string } },
+              public description?: string,
+              public tag_list?: string[],
+              public created_at?: string,
+              public updated_at?: string,) {
+  }
 }
